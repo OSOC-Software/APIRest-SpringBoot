3798a52be53f8d8ef6be4a210c0406a69eaddc8a
JuanDiegoOsoC <juand.osorio@hotmail.com>
2023-03-23 00:40:13 -0500

    Finish Api Spring boot

--- rest/src/main/java/com/alienexplorer/app/rest/Controller/TodoController.java
+++ rest/src/main/java/com/alienexplorer/app/rest/Controller/TodoController.java
@@ -3,9 +3,9 @@ package com.alienexplorer.app.rest.Controller;
 import com.alienexplorer.app.rest.Model.Task;
 import com.alienexplorer.app.rest.Repository.TodoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Collections;
 import java.util.List;
 
 @RestController
@@ -24,4 +24,24 @@ public class TodoController {
         return todoRepository.findAll();
     }
 
+    @PostMapping(value = "/savetask" )
+    public String saveTask(@RequestBody Task task){
+        todoRepository.save(task);
+        return "Save taks";
+    }
+
+    @PutMapping(value = "/update/{id}")
+    public String updateTask(@PathVariable long id ,@RequestBody Task task){
+        Task update = todoRepository.findAllById(Collections.singleton(id)).get(0);
+        update.setTitle(task.getTitle());
+        update.setDescription(task.getDescription());
+        todoRepository.save(update);
+        return "Updated Task";
+    }
+
+    @DeleteMapping(value = "/delete/{id}")
+    public String deleteTask(@PathVariable long id){
+        todoRepository.deleteById(id);
+        return "Task Deleted";
+    }
 }
